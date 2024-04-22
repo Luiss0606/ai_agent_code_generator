55bedd2e348177d06fdad8768313e8fe9e14ca13
Luis Huingo <luis.huingo.c@uni.pe>
2024-04-22 17:51:24 -0500

    Agent functional

---
+++ main.py
@@ -0,0 +1,90 @@
+import os
+from llama_index.llms.ollama import Ollama
+from llama_parse import LlamaParse
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, PromptTemplate
+from llama_index.core.embeddings import resolve_embed_model
+from llama_index.core.tools import QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from pydantic import BaseModel
+from llama_index.core.output_parsers import PydanticOutputParser
+from llama_index.core.query_pipeline import QueryPipeline
+from prompts import context, code_parser_template
+from code_reader import code_reader
+from dotenv import load_dotenv
+import ast
+
+load_dotenv()
+
+llm = Ollama(model='mistral', request_timeout=30.0)
+
+parser = LlamaParse(result_type="markdown")
+
+file_extractor = {".pdf": parser}
+documents = SimpleDirectoryReader("./data", file_extractor=file_extractor).load_data()
+
+embed_model = resolve_embed_model("local:BAAI/bge-m3")
+vector_index = VectorStoreIndex.from_documents(documents, embed_model=embed_model)
+query_engine = vector_index.as_query_engine(llm=llm)
+
+tools = [
+    QueryEngineTool(
+        query_engine=query_engine,
+        metadata=ToolMetadata(
+            name="api_documentation",
+            description="this gives documentation for an api. Use this tool to get documentation for an api.",
+        ),
+    ),
+    code_reader,
+]
+
+
+code_llm = Ollama(model="codellama", request_timeout=30.0)
+agent = ReActAgent.from_tools(
+    tools=tools,
+    llm=code_llm,
+    verbose=True,
+    context=context,
+)
+
+
+class CodeOutput(BaseModel):
+    code: str
+    description: str
+    filename: str
+
+parser = PydanticOutputParser(CodeOutput)
+
+json_prompt_str = parser.format(code_parser_template)
+json_prompt_tmpl = PromptTemplate(json_prompt_str)
+output_pipeline = QueryPipeline(chain=[json_prompt_tmpl], llm=llm)
+
+while (prompt := input("Enter your prompt: "))!= "q":
+    retries = 0
+
+    while retries < 3:
+        try:
+            result = agent.query(prompt)
+            next_result = output_pipeline.run(response=result)
+            cleaned_json = ast.literal_eval(next_result).replace("assistant:", "")
+            break
+        except Exception as e:
+            retries += 1
+            print(f"Error: #{retries}", e)
+    
+    if retries >= 3:
+        print("Unable to generate code. Please try again.")
+        continue
+
+    print('Code generated')
+    print(cleaned_json['code'])
+
+    print('\n\nDescription:', cleaned_json['description'])
+
+    filename = cleaned_json['filename']
+
+    try:
+        with open(os.path.join(filename), "w") as f:
+            f.write(cleaned_json['code'])
+        print("saved file", filename)
+    except Exception as e:
+        print("Error saving code. Please try again.")
